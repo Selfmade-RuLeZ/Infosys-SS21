275b9ef3bccf396fcb017b21c5cace2d44b0f787
Marcel Autenrieth <maauit02@hs-esslingen.de>
2021-06-13 17:35:50 +0200

    initialisierungsskript für tenant tabelle

---
+++ setup_tenant_table.sql
@@ -0,0 +1,13 @@
+insert into Tenant ( First_Name, Last_Name, IBAN, Persons)
+values 
+( 'Marcel', 'Mueller' , 'DE12312312312312312312', 2),
+( 'Peter', 'Maier' , 'DE12312312312312312313', 3),
+( 'Ingrid', 'Haug' , 'DE12312312312312312314', 4),
+( 'Dietmar', 'Haefele' , 'DE12312312312312312315', 2),
+( 'Petar', 'Koslowski' , 'DE12312312312312312316', 1),
+( 'Dietmar', 'Haefele' , 'DE12312312312312312317', 2),
+( 'Luigi', 'Barni' , 'DE12312312312312312318', 1),
+( 'Michael', 'Mann' , 'DE12312312312312312320', 5),
+( 'Günter', 'Gruner' , 'DE12312312312312312321', 2),
+( 'Matthias', 'Stich' , 'DE12312312312312312322', 3)
+;
